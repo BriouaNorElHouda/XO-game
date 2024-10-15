22d3cc2e62e243ea96b63295960ecea3ec4758fe
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2024-10-15 19:21:30 +0100

    Add files via upload

---
+++ XOGame/game_button.dart
@@ -0,0 +1,19 @@
+
+    import 'package:flutter/material.dart';
+    class GameButton {
+
+    late int id;
+    late Color clr;
+    late bool enabled;
+    late String str;
+    GameButton(id){
+
+    this.id= id;
+    this.clr=Colors.deepPurple.shade900;
+    this.enabled =true;
+    this.str='';
+    }
+
+    }
+
+
